aaf8687a0c2bf16cfd3dff16dc86ae45afa3a044
Itisha Maheshwari <itishamaheshwari2005@gmail.com>
2024-09-28 19:12:47 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,59 @@
+// JS is a dynamic typed language
+
+// primitive datatypes (call by value)
+// there are 7
+
+// string
+// boolean
+// number
+// null
+// undefined
+// symbol
+// bigInt
+
+
+// how to define Symbol ??
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id);
+console.log(anotherId);
+
+console.log(id == anotherId);
+
+// here the values passed are same, still it gives different outputs for both of them
+
+
+const bigNumber = 12545251314734n // here adding 'n' in the end, tells the compiler that this a 
+                                  // big number so take it as a BigInt datatype
+console.log(typeof bigNumber);
+console.log(bigNumber);
+
+
+
+
+// reference (non-primitive type)
+
+// arrays
+// objects
+// functions
+
+
+const heroes = ["shaktiman", "naagraj", "spiderman"] //array
+console.log(heroes);
+
+
+let myObj = {        // object
+    name : "Itisha",
+    age : 19,
+    gender : "Female",
+}
+console.log(myObj);
+
+
+const myFunc = function() {      // function
+    console.log("Hello, World!");  
+}
+
+console.log(typeof myFunc);
