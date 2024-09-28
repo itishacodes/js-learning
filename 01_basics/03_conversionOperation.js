1db37209a72c7178f425b5b621b5be2e7ab133aa
Itisha Maheshwari <itishamaheshwari2005@gmail.com>
2024-09-28 17:30:34 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -39,6 +39,48 @@ console.log(typeof stringNumber);
 console.log(stringNumber);
 
 
+// ************* OPERATIONS ************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(typeof negValue);
+console.log(typeof value);
+
+
+let str1 = "Hello";
+let str2 = " Itisha!";
+
+console.log(str1 + str2); // Hello Itisha!
+
+console.log("1" + 2);  // 12 // string + number
+console.log("1" + "2");  // 12 // string + string
+console.log(1 + 2); // 3 // number + number
+
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+console.log((1 + 2) + "2"); // 32
+console.log("1" + (2 + 2)); // 14
+
+// Use paranthesis for calculation preference and avoid confusing syntax
+
+console.log(true); // 1             // '+' is helping in conversion
+console.log(+true); // 1
+// console.log(true+); // error
+
+
+let gameCounter = 100;             // learn prefix and postfix operators
+++gameCounter;
+// gameCounter++
+console.log(gameCounter);
+
+
+
+
+
+
+
+
 
 
 
