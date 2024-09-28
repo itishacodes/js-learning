6d4684f4a796b313f3ffd1d42e19baab7e297666
Itisha Maheshwari <itishamaheshwari2005@gmail.com>
2024-09-28 11:06:38 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict";  // treat all JS code as a newer version
+
+// alert("hello") 
+// node me alert pop-up nhi aata
+// we are using node JS, not browser
+
+let name = "Itisha" // string
+let age = 18 // integer
+let isLoggedIn = false // boolean
+let gender;
+
+// bigInt --> for large numbers
+// null ---> standalone value
+// undefined ---> representation of an empty value
+
+console.table([typeof name, typeof age, typeof isLoggedIn, typeof null, typeof gender])
