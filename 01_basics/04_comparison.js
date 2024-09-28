0c3d239285e1480fdf4f03053e53a48c02a46ad4
Itisha Maheshwari <itishamaheshwari2005@gmail.com>
2024-09-28 17:53:49 +0000

    learning about comparison operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);  // true
+// console.log("02" > 1);  // true
+
+
+// console.log(null > 0);  // false
+// console.log(null == 0);  // false
+// console.log(null >= 0);  // true
+
+
+// console.log(undefined = 0);  // 0
+// console.log(undefined > 0);  // false
+// console.log(undefined < 0);  // false
+
+
+console.log("2" == 2); // true
+// here it gives 'true' as it converts string into number for comparison
+
+console.log("2" === 2); // false
+// to compare it without any conversion, we use 'strict check' ie. '==='
+// now it checks datatype
+
+
+
+
